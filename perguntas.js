b42198dfe9fdc40a3e2986515a08744cd1791416
amandinhaisrael <103213836+amandinhaisrael@users.noreply.github.com>
2024-09-25 09:29:26 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -38,7 +38,6 @@ const perguntas = [
                     }
                      
         ]
-
     },
     {
         enunciado: "você sabe qual é a proposta da ONU que protege o meio ambiente?",
@@ -88,7 +87,6 @@ const perguntas = [
                     "as arvores que protegem o meio ambiente, pois se não tivessem elas as faricas tomariam conta do oxigenio.",
                     "se cada aniversário que você fizesse, uma árvore fosse plantada o ambiente seria uito melhor "
                 ]
-
                 },
                  {
                      texto: "Não, pois não vejo o porque.",
